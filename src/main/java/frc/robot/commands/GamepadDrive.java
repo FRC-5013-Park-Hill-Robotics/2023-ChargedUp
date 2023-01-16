73ddeab625614c9033b05ba4d25536917faaa789
Tyler Dinh <tylerpokemon3@gmail.com>
2023-01-16 14:24:42 -0600

    Create & update new classes/command/subsystems

---
+++ src/main/java/frc/robot/commands/GamepadDrive.java
@@ -0,0 +1,99 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.filter.SlewRateLimiter;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.LogitechController;
+import static frc.robot.constants.GlobalConstants.*;
+
+public class GamepadDrive extends CommandBase {
+	private Drivetrain m_drivetrain;
+	private LogitechController m_gamepad;
+	private SlewRateLimiter xLimiter = new SlewRateLimiter(3);
+	private SlewRateLimiter yLimiter = new SlewRateLimiter(3);
+	private SlewRateLimiter rotationLimiter = new SlewRateLimiter(3);
+
+	/**
+	 * Constructor method for the GamepadDrive class
+	 * - Creates a new GamepadDrive object.
+	 */
+	public GamepadDrive(Drivetrain drivetrain, LogitechController gamepad) {
+		super();
+		addRequirements(drivetrain);
+		m_gamepad = gamepad;
+		m_drivetrain = drivetrain;
+	}
+
+	@Override
+	public void execute() {
+		double throttle = modifyAxis(m_gamepad.getRightTriggerAxis());
+
+		double translationX = modifyAxis(-m_gamepad.getLeftY());
+		double translationY = modifyAxis(-m_gamepad.getLeftX());
+		if (!(translationX == 0.0 && translationY == 0.0)) {
+			
+			double angle = calculateTranslationDirection(translationX, translationY);
+			translationX = Math.cos(angle) * throttle;
+			translationY = Math.sin(angle) * throttle;
+		}
+
+		m_drivetrain.drive(ChassisSpeeds.fromFieldRelativeSpeeds(
+				-Drivetrain.percentOutputToMetersPerSecond(xLimiter.calculate(translationX)),
+				Drivetrain.percentOutputToMetersPerSecond(yLimiter.calculate(translationY)), getRotationRadiansPerSecond(),
+				m_drivetrain.getYawR2d()));
+
+		/*
+		 * m_drivetrain.drive(ChassisSpeeds.fromFieldRelativeSpeeds(
+		 * getXTranslationMetersPerSecond(),
+		 * getYTranslationMetersPerSecond(), getRotationRadiansPerSecond(),
+		 * m_drivetrain.getGyroscopeRotation()));
+		 */ }
+
+	@Override
+	public void end(boolean interrupted) {
+		m_drivetrain.drive(new ChassisSpeeds(0.0, 0.0, 0.0));
+	}
+
+	private double getXTranslationMetersPerSecond() {
+		// on the controller y is up, on the field x is away from the driver
+		return -Drivetrain
+				.percentOutputToMetersPerSecond(xLimiter.calculate(modifyAxis(m_gamepad.getLeftY())));
+	}
+
+	private double getYTranslationMetersPerSecond() {
+		// on the controller y is up, on the field x is away from the driver
+		return -Drivetrain
+				.percentOutputToMetersPerSecond(yLimiter.calculate(modifyAxis(m_gamepad.getLeftX())));
+	}
+
+	private double getRotationRadiansPerSecond() {
+		return -Drivetrain
+				.percentOutputToRadiansPerSecond(rotationLimiter.calculate(modifyAxis(m_gamepad.getRightX(),2))) / 3;
+
+	}
+
+	private static double modifyAxis(double value) {
+	
+		return modifyAxis(value, 1);
+	}
+	private static double modifyAxis(double value, int exponent) {
+		// Deadband
+		value = MathUtil.applyDeadband(value, ControllerConstants.DEADBAND);
+
+		 value = Math.copySign(Math.pow(value, exponent), value);
+
+		return value;
+	}
+	private double calculateTranslationDirection(double x, double y) {
+		// Calculate the angle.
+		// Swapping x/y
+		return Math.atan2(x, y) + Math.PI / 2;
+	}
+}
---
+++ src/main/java/frc/robot/constants/DrivetrainConstants.java
@@ -0,0 +1,114 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.constants;
+
+import com.swervedrivespecialties.swervelib.CanPort;
+import com.swervedrivespecialties.swervelib.SdsModuleConfigurations;
+import com.swervedrivespecialties.swervelib.Mk4SwerveModuleHelper.GearRatio;
+
+import frc.robot.constants.CANConstants.FrontRightSwerveConstants;
+import frc.robot.constants.CANConstants.BackLeftSwerveConstants;
+import frc.robot.constants.CANConstants.BackRightSwerveConstants;
+import static frc.robot.constants.CANConstants.*;
+
+/** Add your docs here. */
+public final class DrivetrainConstants {
+    public static final CanPort PIGEON_ID = new CanPort(14,GlobalConstants.CANIVORE_NAME);
+    public static final GearRatio SWERVE_GEAR_RATIO = GearRatio.L2;
+    /**
+     * The maximum voltage that will be delivered to the drive motors.
+     * <p>
+     * This can be reduced to cap the robot's maximum speed. Typically, this is
+     * useful during initial testing of the robot.
+     */
+    public static final double MAX_VOLTAGE = 12.0;
+
+    public static final class DrivetrainGeometry {
+
+        /**
+         * The left-to-right distance between the drivetrain wheels Should be measured
+         * from center to center.
+         */
+        public static final double TRACKWIDTH_METERS = .585;
+        /**
+         * The front-to-back distance between the drivetrain wheels. Should be measured
+         * from center to center.
+         */
+        public static final double WHEELBASE_METERS = .585;
+
+        /**
+         * The maximum velocity of the robot in meters per second.
+         * <p>
+         * This is a measure of how fast the robot should be able to drive in a straight
+         * line.
+         */
+        public static final double MAX_VELOCITY_METERS_PER_SECOND = GlobalConstants.FALCON_500_MAX_RPM / 60.0
+                * SdsModuleConfigurations.MK4_L2.getDriveReduction()
+                * SdsModuleConfigurations.MK4_L2.getWheelDiameter()
+                * Math.PI;
+                //* .69;
+        /**
+         * The maximum angular velocity of the robot in radians per second.
+         * <p>
+         * This is a measure of how fast the robot can rotate in place.
+         */
+        public static final double MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND = MAX_VELOCITY_METERS_PER_SECOND
+                / Math.hypot(TRACKWIDTH_METERS / 2.0, WHEELBASE_METERS / 2.0);
+    }
+
+    public static final class FrontLeftSwerveConstants {
+        public static final CanPort STEER_MOTOR_ID = new CanPort(FRONT_LEFT_DRIVE_MOTOR_ID,GlobalConstants.CANIVORE_NAME);
+        public static final CanPort DRIVE_MOTOR_ID = new CanPort(3,GlobalConstants.CANIVORE_NAME);
+        public static final CanPort ENCODER_ID = new CanPort(4,GlobalConstants.CANIVORE_NAME);
+        public static final double ENCODER_OFFSET_RADIANS =  -Math.toRadians(87.2);            
+        public static final int STATES_INDEX = 0;
+    }
+
+    public static final class FrontRightSwerveConstants {
+        public static final CanPort STEER_MOTOR_ID = new CanPort(5,GlobalConstants.CANIVORE_NAME);
+        public static final CanPort DRIVE_MOTOR_ID = new CanPort(6,GlobalConstants.CANIVORE_NAME);
+        public static final CanPort ENCODER_ID = new CanPort(7,GlobalConstants.CANIVORE_NAME);
+        public static final double ENCODER_OFFSET_RADIANS = -Math.toRadians(1.45);
+        public static final int STATES_INDEX = 1;
+    }
+
+    public static final class BackLeftSwerveConstants {
+        public static final CanPort STEER_MOTOR_ID = new CanPort(8,GlobalConstants.CANIVORE_NAME);
+        public static final CanPort DRIVE_MOTOR_ID = new CanPort(9,GlobalConstants.CANIVORE_NAME);
+        public static final CanPort ENCODER_ID = new CanPort(10,GlobalConstants.CANIVORE_NAME);
+        public static final double ENCODER_OFFSET_RADIANS = -Math.toRadians(87.7);
+        public static final int STATES_INDEX = 2;
+    }
+
+    public static final class BackRightSwerveConstants {
+        public static final CanPort STEER_MOTOR_ID = new CanPort(11,GlobalConstants.CANIVORE_NAME);
+        public static final CanPort DRIVE_MOTOR_ID = new CanPort(12,GlobalConstants.CANIVORE_NAME);
+        public static final CanPort ENCODER_ID = new CanPort(13,GlobalConstants.CANIVORE_NAME);
+        public static final double ENCODER_OFFSET_RADIANS =  -Math.toRadians(312.5);
+        public static final int STATES_INDEX = 3;
+    }
+
+    // Turning the bot gains used by PIDControllers
+    public static final class ThetaGains {
+        public static final double kP = 4;
+        public static final double kI = 0;
+        public static final double kD = .05;
+        public static final double kTurnToleranceRad = 0.05;
+        public static final double kTurnRateToleranceRadPerS = .25;
+    }
+
+    // Driving the bot gains used by PIDControllers
+    public static final class TranslationGains {
+       // public static final double kP = 2.2956;
+       public static final double kP = 0.15;
+        public static final double kI = 0;
+        public static final double kD = 0;
+        public static final double kA = 0.16089;
+        public static final double kV = 2.2528;
+        public static final double kS = 0.60483;
+    }
+    //
+
+}
