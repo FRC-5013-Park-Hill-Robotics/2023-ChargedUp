127328720a249fbb0d19e9f943e17ddb763be544
adira5013 <91989833+adira5013@users.noreply.github.com>
2023-01-23 16:12:23 -0600

    photon vis stuff

---
+++ src/main/java/frc/robot/constants/PhotonVisionConstants.java
@@ -0,0 +1,12 @@
+package frc.robot.constants;
+
+
+
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
+
+public final class PhotonVisionConstants{
+    public static Transform3d robotToCam1 = new Transform3d(new Translation3d(0.5, 0.0, 0.5), new Rotation3d(0,0,0));
+    public static Transform3d robotToCam2 = new Transform3d(new Translation3d(0.5, 0.0, 0.5), new Rotation3d(0,0,0));  
+}
--- src/main/java/frc/robot/subsystems/PhotonVision.java
+++ src/main/java/frc/robot/subsystems/PhotonVision.java
@@ -15,6 +15,7 @@ import org.photonvision.RobotPoseEstimator;
 import org.photonvision.RobotPoseEstimator.PoseStrategy;
 
 import frc.robot.RobotContainer;
+import frc.robot.constants.PhotonVisionConstants;
 
 
 
@@ -24,7 +25,7 @@ public class PhotonVision extends SubsystemBase{
     private PhotonCamera m_rearCamera = new PhotonCamera("backcamera");
     private RobotContainer m_robotContainer;
 
-    Transform3d robotToCam = new Transform3d(new Translation3d(0.5, 0.0, 0.5), new Rotation3d(0,0,0)); 
+    
 
     ArrayList<Pair<PhotonCamera, Transform3d>> camList = new ArrayList<Pair<PhotonCamera, Transform3d>>();
 
@@ -35,7 +36,8 @@ public class PhotonVision extends SubsystemBase{
 
     public PhotonVision(){
         super();
-        camList.add(new Pair<PhotonCamera, Transform3d>(m_frontCamera, robotToCam));
+        camList.add(new Pair<PhotonCamera, Transform3d>(m_frontCamera, PhotonVisionConstants.robotToCam1));
+        camList.add(new Pair<PhotonCamera, Transform3d>(m_rearCamera, PhotonVisionConstants.robotToCam2));
         try{
             aprilTagFieldLayout = new AprilTagFieldLayout(AprilTagFields.k2022RapidReact.m_resourceFile);
         } catch (IOException e){
